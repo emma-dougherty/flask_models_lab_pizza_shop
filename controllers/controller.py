03a6325e6eb583bca5620fd98e44f85ac002d32f
andrewredman91 <andrewredman91@gmail.com>
2022-04-13 15:36:58 +0100

    second commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,13 +4,10 @@ from models.order_list import orders
 
 @app.route('/orders')
 def index():
-    return render_template('index.html', title="Pizza Orders", order_list=orders )
+    return render_template('index.html', title="Pizza Orders", order_list= orders)
 
 @app.route('/orders/<index>')
 def order(index):
     order = orders[int(index)]
     return render_template('order.html', title=f"Order no: {order}", order = order)
 
-# #@app.route('/orders/<index>')
-# def order(order):
-#     return render_template('order.html', title=f"Order no: {order}")
