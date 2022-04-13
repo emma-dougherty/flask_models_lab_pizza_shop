ae0603982f8b3e61e8c3de3912aad4614f17db49
andrewredman91 <andrewredman91@gmail.com>
2022-04-13 15:06:52 +0100

    first commit

---
+++ controllers/controller.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+from models.order_list import orders
+
+@app.route('/orders')
+def index():
+    return render_template('index.html', title="Pizza Orders", order_list=orders )
+
+@app.route('/orders/<index>')
+def order(index):
+    order = orders[int(index)]
+    return render_template('order.html', title=f"Order no: {order}", order = order)
+
+# #@app.route('/orders/<index>')
+# def order(order):
+#     return render_template('order.html', title=f"Order no: {order}")
---
+++ models/order_list.py
@@ -0,0 +1,8 @@
+from models.order import *
+
+
+
+order1 = Order("Bill Connelly", "13/04/2022 12:06", 2)
+order2 = Order("Palm Stevenson", "13/04/2022 13:06", 5)
+
+orders = [order1, order2]
